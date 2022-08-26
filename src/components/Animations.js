dfce2a02bedc94c516434439610b24045961ce29
Eros D <gomezeros@gmail.com>
2022-08-26 17:42:22 -0300

    Reorg 1 - Componentes

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -2,11 +2,47 @@ $(function(){
     var $menu = $('.dropdown-menu');
   
     $('.dropdown-toggle').hover(
-      function() {
+      function dropDownToggle() {
         $menu.css('opacity',1);
       },
-      function() {
+      function dropDownToggle() {
         $menu.css('opacity',0);
       });
-  })();
+})();
+
   
+
+document.addEventListener("DOMContentLoaded", function(){
+  // make it as accordion for smaller screens
+  if (window.innerWidth < 992) {
+  
+    // close all inner dropdowns when parent is closed
+    document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+      everydropdown.addEventListener('hidden.bs.dropdown', function () {
+        // after dropdown is hidden, then find all submenus
+          this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+            // hide every submenu as well
+            everysubmenu.style.display = 'none';
+          });
+      })
+    });
+  
+    document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+      element.addEventListener('click', function (e) {
+          let nextEl = this.nextElementSibling;
+          if(nextEl && nextEl.classList.contains('submenu')) {	
+            // prevent opening link if link needs to open dropdown
+            e.preventDefault();
+            if(nextEl.style.display == 'block'){
+              nextEl.style.display = 'none';
+            } else {
+              nextEl.style.display = 'block';
+            }
+  
+          }
+      });
+    })
+  }
+  // end if innerWidth
+  }); 
+  // DOMContentLoaded  end
