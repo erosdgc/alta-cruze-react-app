892bcc72a3920eb5b80e14961b68d3122780bab5
Eros D <gomezeros@gmail.com>
2022-10-03 23:29:00 -0300

    Several components deletion

--- src/components/ItemList/ItemList copy.jsx
+++
@@ -1,8 +0,0 @@
-import Item from "../Item/Item";
-import React from "react";
-
-export default function ItemList ({data = []}) {
-    return (
-        data.map (cruise => <Item key={cruise.id} item={cruise} />)
-    );
-}
